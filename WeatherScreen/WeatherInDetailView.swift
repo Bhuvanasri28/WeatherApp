e05d063c94b7e6a515c1c3f1c87ee2c2d7c1aace
Bhuvanasri28 <bhuvanaravuri@gmail.com>
2024-11-05 17:00:41 -0500

    Adding extra grid and removing some data

--- WeatherScreen/WeatherInDetailView.swift
+++ WeatherScreen/WeatherInDetailView.swift
@@ -12,28 +12,10 @@ struct WeatherInDetailView: View {
     var viewModel: WeatherViewModel
     
     var body: some View {
-        
-        //        if let weather = viewModel.weather {
-        //            Text("Visibility: \(weather.visibilityInKm)")
-        //                .foregroundColor(.white)
-        //                .font(.system(size: 20))
-        //
-        //            Text("Wind: \(String(format: "%.1f", weather.windSpeed)) m/s \(weather.windDirectionCompass)")
-        //                .foregroundColor(.white)
-        //                .font(.system(size: 20))
-        //
-        //            Text("Rain: \(weather.formattedRainVolume)")
-        //                .foregroundColor(.white)
-        //                .font(.system(size: 20))
-        //
-        //            Text("Cloud Coverage: \(weather.cloudCoverageDescription)")
-        //                .foregroundColor(.white)
-        //                .font(.system(size: 20))
-        //        }
         if let weather = viewModel.weather {
             LazyVGrid(columns: columns) {
                 VStack(alignment: .leading) {
-                    Text("\(Image(systemName: "humidity"))  FEELS LIKE")
+                    Text("\(Image(systemName: "humidity")) HUMIDITY")
                         .bold()
                         .font(.system(size: 15, weight: .medium, design: .default))
                         .foregroundColor(.white)
@@ -114,6 +96,25 @@ struct WeatherInDetailView: View {
             }
             .padding()
             .frame(height: 200)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.blue.opacity(0.2))
+            .cornerRadius(15)
+            VStack(alignment: .leading) {
+                Text("\(Image(systemName: "cloud.rain")) RAIN")
+                    .font(.system(size: 15, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding(.bottom)
+                Text("\(weather.formattedRainVolume)")
+                    .bold()
+                    .font(.system(size: 25, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding(.bottom)
+                Text("Cloud Coverage: \(weather.cloudCoverageDescription)")
+                    .foregroundColor(.white)
+                    .font(.system(size: 15))
+            }
+            .padding()
+            .frame(height: 150)
             .frame(maxWidth: .infinity, alignment: .leading)
             .background(Color.blue.opacity(0.2))
             .cornerRadius(15)
